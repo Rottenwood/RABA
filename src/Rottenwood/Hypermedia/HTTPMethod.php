b36acbc5f81d627eb83811b898418451638a0960
Petr Karmashёv <smonkl@bk.ru>
2015-05-13 21:52:22 +0300

    Константы HTTP-методов

---
+++ src/Rottenwood/Hypermedia/HTTPMethod.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rottenwood\Hypermedia;
+
+class HTTPMethod
+{
+    const GET = 'GET';
+    const POST = 'POST';
+    const OPTIONS = 'OPTIONS';
+    const HEAD = 'HEAD';
+    const PUT = 'PUT';
+    const DELETE = 'DELETE';
+    const TRACE = 'TRACE';
+    const CONNECT = 'CONNECT';
+}
