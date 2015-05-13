e7a6fb5c64b90c263e8e705f018947d7534c1ae3
Petr Karmashёv <smonkl@bk.ru>
2015-05-13 19:55:55 +0300

    REST сервис для бронирования

---
+++ README.md
@@ -0,0 +1,4 @@
+booking
+=======
+
+A Symfony project created on May 13, 2015, 7:17 pm.
---
+++ src/Rottenwood/BookingBundle/RottenwoodBookingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rottenwood\BookingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RottenwoodBookingBundle extends Bundle
+{
+}
